f366cd684876572e94b52457d4fc0f06cc48aba3
thomas <tg296@.mit.edu>
2017-05-11 17:29:46 -0400

    Added __init__ function to heterozygosity.py to allow it to be used both as a script and as a module (e.g. for multiprocessing)

--- heterozygosity.py
+++ heterozygosity.py
@@ -26,7 +26,7 @@ Usage:
             python heterozygosity.py -i input_sorted.sam -s sampleID -o output_dir 
 
 
-2.  (BETWEEN SAMPLE HETEROZYGOSITY) 
+2.  (BETWEEN SAMPLE HETEROZYGOSITY)
 
           i.  Compute SNP loci from folder of kpileups for all samples:
 
@@ -36,10 +36,9 @@ Usage:
 
               python heterozygosity.py -bw True -p True -i /path/to/sam/folders -o /path/to/precomputed/startlines
 
-        iii.  Run each pair of samples as SORTED sam files:
+        iii.  Run each pair of samples as SORTED sam files of interest, either using a multiprocessing pool (function: parse_two_sam_files_multiprocessing()), or as a script as follows:
 
               python heterozygosity.py -bw True -i1 input_sorted_1.sam -i2 input_sorted_2.sam -ip /path/to/precomputed/startlines -s1 sampleID_1 -s2 sampleID_2 -o output_dir
-
 
 """
 
@@ -451,6 +450,190 @@ def parse_two_sam_files(samfile1, samfile2, snp_loci, precomputed_startlines_fol
     return heterozygosity_dict, readcount_dict_1, readcount_dict_2
 
 
+def parse_two_sam_files_multiprocessing(samfile1, samfile2, snp_loci, precomputed_startlines_folder, sampleID1, sampleID2, results_dict):
+    # Parses two sorted alignments SAM files for overlapping positions
+    # Used for multiprocessing calls using pool.apply_async() from a multiprocessing pool.  Requires 'results_dict' to be a manager dict(), i.e. global across the pool.
+
+    # -for each read, extracts gene it maps to, alignment position start and finish
+    with open(samfile1, 'r') as fid:
+        all_lines_1 = fid.readlines()
+    with open(samfile2, 'r') as fid:
+        all_lines_2 = fid.readlines()
+    
+    # Get start lines for each gene in both files
+    samfile1_basename = samfile1.split('/')[-1]
+    samfile2_basename = samfile2.split('/')[-1]
+    startlines_dict_1 = cPickle.load(open(os.path.join(precomputed_startlines_folder, samfile1_basename + '.pkl'), 'rb'))
+    startlines_dict_2 = cPickle.load(open(os.path.join(precomputed_startlines_folder, samfile2_basename + '.pkl'), 'rb'))
+    gene_firstlines_1 = startlines_dict_1['gene_firstlines']
+    genes_counted_1 = startlines_dict_1['genes_counted']
+    gene_firstlines_2 = startlines_dict_2['gene_firstlines']
+    genes_counted_2 = startlines_dict_2['genes_counted']
+
+    # Look for read indices of genes of interest
+    good_indices_1 = {}
+    good_indices_2 = {}
+    genes_of_interest = snp_loci.keys()
+    for gene in genes_of_interest:
+        if gene in genes_counted_1:
+            good_indices_1[gene] = genes_counted_1.index(gene)
+        if gene in genes_counted_2:
+            good_indices_2[gene] = genes_counted_2.index(gene)
+
+    # Extract overlapping read pairs for each gene, and compute total read count per gene
+    heterozygosity_dict = {}
+    readcount_dict_1 = {}
+    readcount_dict_2 = {}
+    counter_1 = 0
+
+    # Loop through each gene in SAM file 1 and find equivalent gene in SAM file 2
+    for gene in good_indices_1:
+
+        gene_line_index_1 = good_indices_1[gene]
+
+        try:
+            gene_line_index_2 = good_indices_2[gene]
+
+            counter_1 += 1
+
+            print "Computing " + str(counter_1) + " of " + str(len(good_indices_1))
+            geneID_1 = genes_counted_1[gene_line_index_1]
+            geneID_2 = genes_counted_2[gene_line_index_2]
+
+            if geneID_1 == 'PN:bwa':
+                continue
+            if geneID_2 == 'PN:bwa':
+                continue
+
+            bact_1 = '_'.join(geneID_1.split(';')[0].split('_')[:2])
+            gene_name_1 = geneID_1.split(';')[0].split('_')[2]
+            firstline_1 = gene_firstlines_1[gene_line_index_1]
+            if counter_1 < len(gene_firstlines_1)-1:
+                lastline_1 = gene_firstlines_1[gene_line_index_1 + 1]
+            else:
+                lastline_1 = len(all_lines_1)
+            # Compute total number of reads for that gene
+            if bact_1 not in readcount_dict_1:
+                readcount_dict_1[bact_1] = {}
+            if gene_name_1 not in readcount_dict_1[bact_1]:
+                readcount_dict_1[bact_1][gene_name_1] = lastline_1 - firstline_1 + 1
+
+            bact_2 = '_'.join(geneID_2.split(';')[0].split('_')[:2])
+            gene_name_2 = geneID_2.split(';')[0].split('_')[2]
+            firstline_2 = gene_firstlines_2[gene_line_index_2]
+            if counter_1 < len(gene_firstlines_2)-1:
+                lastline_2 = gene_firstlines_2[gene_line_index_2 + 1]
+            else:
+                lastline_2 = len(all_lines_2)
+            # Compute total number of reads for that gene
+            if bact_2 not in readcount_dict_2:
+                readcount_dict_2[bact_2] = {}
+            if gene_name_2 not in readcount_dict_2[bact_2]:
+                readcount_dict_2[bact_2][gene_name_2] = lastline_2 - firstline_2 + 1   
+
+            # Check that both bacteria and genes are the same
+            if bact_1 != bact_2 or gene_name_1 != gene_name_2:
+                raise NameError('Gene IDs ' + str(geneID_1) + ' and ' + str(geneID_2) + ' do not match!')
+
+            # Get read positions on the gene
+            filtered_reads = {}
+            for snp_position_of_interest in snp_loci[geneID_1]:
+
+                read_positions_1 = {}
+                read_positions_2 = {}
+
+                # SAM file 1
+                for j in range(firstline_1, lastline_1):
+                    line = all_lines_1[j]
+                    line = line.rstrip().split()
+                    if len(line) > 3:
+                        readID = line[0]
+                        start_position = int(line[3])
+                        end_position = start_position + len(line[9]) - 1  # mapped sequence = line[9]; not allocated for speed purposes
+                        read_positions_1[readID] = [start_position, end_position, j]
+
+                # SAM file 2
+                for j in range(firstline_2, lastline_2):
+                    line = all_lines_2[j]
+                    line = line.rstrip().split()
+                    if len(line) > 3:
+                        readID = line[0]
+                        start_position = int(line[3])
+                        end_position = start_position + len(line[9]) - 1  # mapped sequence = line[9]; not allocated for speed purposes
+                        read_positions_2[readID] = [start_position, end_position, j]
+
+                # Get all genotypes and only consider this SNP further if there are at least 2 counts of each allele
+                readIDs_1 = read_positions_1.keys()
+                readIDs_2 = read_positions_2.keys()
+                snp_genotypes_1 = []
+                snp_genotypes_2 = []
+                for m in range(len(readIDs_1)):
+                    readID = readIDs_1[m]
+                    if read_positions_1[readID][0] <= snp_position_of_interest and read_positions_1[readID][1] >= snp_position_of_interest:
+                        read_line = all_lines_1[read_positions_1[readID][2]].rstrip().split()
+                        read = read_line[9]
+                        read_start = int(read_line[3])
+                        read_snp_index =  snp_position_of_interest - read_start
+                        snp_genotypes_1.append(read[read_snp_index])
+                for m in range(len(readIDs_2)):
+                    readID = readIDs_2[m]
+                    if read_positions_2[readID][0] <= snp_position_of_interest and read_positions_2[readID][1] >= snp_position_of_interest:
+                        read_line = all_lines_2[read_positions_2[readID][2]].rstrip().split()
+                        read = read_line[9]
+                        read_start = int(read_line[3])
+                        read_snp_index =  snp_position_of_interest - read_start
+                        snp_genotypes_2.append(read[read_snp_index])
+                alleles_1 = np.unique(snp_genotypes_1)
+                alleles_2 = np.unique(snp_genotypes_2)
+
+                # If more than one allele, check for counts and only include alleles with more than 1 count.
+                good_alleles_1 = []
+                good_alleles_2 = []
+                if len(alleles_1) > 0:
+                    for allele in alleles_1:
+                        if snp_genotypes_1.count(allele) > 1:
+                            good_alleles_1.append(allele)
+                if len(alleles_2) > 0:
+                    for allele in alleles_2:
+                        if snp_genotypes_2.count(allele) > 1:
+                            good_alleles_2.append(allele)
+
+                # Case 1: both have only one 'good' allele that passes the filter
+                if len(good_alleles_1) == 1 and len(good_alleles_2) == 1:
+                    if bact_1 not in heterozygosity_dict:
+                        heterozygosity_dict[bact_1] = {}
+                    if gene_name_1 not in heterozygosity_dict[bact_1]:
+                        heterozygosity_dict[bact_1][gene_name_1] = {}
+                    if good_alleles_1[0] == good_alleles_2[0]:
+                        heterozygosity_dict[bact_1][gene_name_1][snp_position_of_interest] = 0.0
+                    else:
+                        heterozygosity_dict[bact_1][gene_name_1][snp_position_of_interest] = 1.0
+
+                # Case 2: either one or both samples have more than one allele that pass the filter
+                if len(good_alleles_1) > 1 or len(good_alleles_1) > 1:
+                    if bact_1 not in heterozygosity_dict:
+                        heterozygosity_dict[bact_1] = {}
+                    if gene_name_1 not in heterozygosity_dict[bact_1]:
+                        heterozygosity_dict[bact_1][gene_name_1] = {}
+
+                    allele_freqs_1 = {allele: snp_genotypes_1.count(allele)/float(len(snp_genotypes_1)) for allele in good_alleles_1}
+                    allele_freqs_2 = {allele: snp_genotypes_2.count(allele)/float(len(snp_genotypes_2)) for allele in good_alleles_2}
+
+                    probability_of_agreement = 0.0
+                    for allele in allele_freqs_1:
+                        if allele in allele_freqs_2:
+                            probability_of_agreement += allele_freqs_1[allele] * allele_freqs_2[allele]
+                    heterozygosity_dict[bact_1][gene_name_1][snp_position_of_interest] = 1.0 - probability_of_agreement
+
+                # Case 3: one or both samples have no allele; do not compute heterozygosity
+                if len(good_alleles_1) == 0 or len(good_alleles_1) == 0:
+                    continue
+        except:
+            continue
+
+    sample_pair = sampleID1 + '-' + sampleID2
+    results_dict[sample_pair] = {'Hz': heterozygosity_dict, 'readcount_dict_1': readcount_dict_1, 'readcount_dict_2': readcount_dict_2}
+    
 def parse_sam_file_individual_snps(samfile, kpileups_file):
     # Parses a sorted alignments SAM file for overlapping positions
     # -for each read, extracts gene it maps to, alignment position start and finish
@@ -567,46 +750,48 @@ def parse_sam_file_individual_snps(samfile, kpileups_file):
                 snp_hz_dict[gene] = hz_vals 
     return heterozygosity_dict, readcount_dict, snp_hz_dict, snp_dict
 
-# MAIN
 
-# Get polymorphic loci
-if args.l == 'True':
-    print "Computing SNP loci from kpileups folder " + str(args.k)
-    snp_loci = build_snp_positions_folder(args.k)
-    cPickle.dump(snp_loci, open('snp_loci.pkl','wb'))
-elif args.bw == 'False':
-    print "Computing SNP heterozygosities for sample " + str(args.s)
-    if args.sl:
-        snp_loci = cPickle.load(open(args.sl,'rb'))
-    else:
-        snp_loci = cPickle.load(open('snp_loci.pkl','rb'))
-    hz_dict, readcount_dict = parse_sam_file(args.i, snp_loci)
-    if not os.path.exists(args.o):
-        os.system('mkdir ' + str(args.o))
-    cPickle.dump(hz_dict, open(os.path.join(args.o, args.s + '_hz.pkl'),'wb'))
-    cPickle.dump(readcount_dict, open(os.path.join(args.o, args.s + '_readcounts.pkl'),'wb'))
-elif args.bw == 'True':
-    if args.p == 'True':
-        print "Precomputing gene start lines in all SAM files for between sample heterozygosity calculations."
-        samfiles = os.listdir(args.i)
-        if not os.path.exists(args.o):
-            os.system('mkdir ' + str(args.o))
-        startline_counter = 0
-        for filename in samfiles:
-            results_dict = precompute_samfile_startlines(os.path.join(args.i, filename))
-            cPickle.dump(results_dict, open(os.path.join(args.o, filename + '.pkl'), 'wb'))
-            startline_counter += 1
-            print "Computed start lines for " + str(startline_counter) + " of " + str(len(samfiles)) + " files."
-        
-    elif args.p == 'False':
-        print "Computing SNP heterozygosities between samples " + str(args.s1) + " and " + str(args.s2)
+# MAIN
+if __name__ == "__main__":
+
+    # Get polymorphic loci
+    if args.l == 'True':
+        print "Computing SNP loci from kpileups folder " + str(args.k)
+        snp_loci = build_snp_positions_folder(args.k)
+        cPickle.dump(snp_loci, open('snp_loci.pkl','wb'))
+    elif args.bw == 'False':
+        print "Computing SNP heterozygosities for sample " + str(args.s)
         if args.sl:
             snp_loci = cPickle.load(open(args.sl,'rb'))
         else:
             snp_loci = cPickle.load(open('snp_loci.pkl','rb'))
-        hz_dict, readcount_dict_1, readcount_dict_2 = parse_two_sam_files(args.i1, args.i2, snp_loci, args.ip)
+        hz_dict, readcount_dict = parse_sam_file(args.i, snp_loci)
         if not os.path.exists(args.o):
             os.system('mkdir ' + str(args.o))
-        cPickle.dump(hz_dict, open(os.path.join(args.o, args.s1 + '_' + args.s2 + '_hz.pkl'),'wb'))
-        cPickle.dump(readcount_dict_1, open(os.path.join(args.o, args.s1 + '_readcounts.pkl'),'wb'))
-        cPickle.dump(readcount_dict_1, open(os.path.join(args.o, args.s2 + '_readcounts.pkl'),'wb'))
+        cPickle.dump(hz_dict, open(os.path.join(args.o, args.s + '_hz.pkl'),'wb'))
+        cPickle.dump(readcount_dict, open(os.path.join(args.o, args.s + '_readcounts.pkl'),'wb'))
+    elif args.bw == 'True':
+        if args.p == 'True':
+            print "Precomputing gene start lines in all SAM files for between sample heterozygosity calculations."
+            samfiles = os.listdir(args.i)
+            if not os.path.exists(args.o):
+                os.system('mkdir ' + str(args.o))
+            startline_counter = 0
+            for filename in samfiles:
+                results_dict = precompute_samfile_startlines(os.path.join(args.i, filename))
+                cPickle.dump(results_dict, open(os.path.join(args.o, filename + '.pkl'), 'wb'))
+                startline_counter += 1
+                print "Computed start lines for " + str(startline_counter) + " of " + str(len(samfiles)) + " files."
+
+        elif args.p == 'False':
+            print "Computing SNP heterozygosities between samples " + str(args.s1) + " and " + str(args.s2)
+            if args.sl:
+                snp_loci = cPickle.load(open(args.sl,'rb'))
+            else:
+                snp_loci = cPickle.load(open('snp_loci.pkl','rb'))
+            hz_dict, readcount_dict_1, readcount_dict_2 = parse_two_sam_files(args.i1, args.i2, snp_loci, args.ip)
+            if not os.path.exists(args.o):
+                os.system('mkdir ' + str(args.o))
+            cPickle.dump(hz_dict, open(os.path.join(args.o, args.s1 + '_' + args.s2 + '_hz.pkl'),'wb'))
+            cPickle.dump(readcount_dict_1, open(os.path.join(args.o, args.s1 + '_readcounts.pkl'),'wb'))
+            cPickle.dump(readcount_dict_1, open(os.path.join(args.o, args.s2 + '_readcounts.pkl'),'wb'))
